204cac6b717d8f943d4478136a5e440d3e17c992
Ash <73482956+ascopes@users.noreply.github.com>
2025-10-17 08:58:30 +0100

    Refactor build script for improved clarity and efficiency

--- build.sh
+++ build.sh
@@ -1,30 +1,22 @@
 #!/usr/bin/env bash
-set -eu
+set -o errexit
+set -o nounset
+[[ -v DEBUG ]] && set -o xtrace
 
 echo "Ensuring tooling is installed..."
-if ! command -v hostlist-compiler &> /dev/null; then
-  npm i -g @adguard/hostlist-compiler
-fi
+command -v hostlist-compiler &> /dev/null \
+    || npm install -g @adguard/hostlist-compiler
 
 # Set the version number in the config.
-echo "Configuring version in config..."
-jq '. * {version: $version}' config.json \
-    --arg version "$(date +'%y.%m.%d.%H%M%S')" \
-    > config.json.new
-mv config.json{.new,}
+version=$(date +'%y.%m.%d.%H%M%S')
+echo "Configuring version in config to be ${version}..."
+jq '. * {version: $version}' config.json --arg version "${version}" > config.json.new
+mv -v config.json{.new,}
 
 # Compile the lists
 echo "Compiling the lists..."
 mkdir -p out/
 
-cmd=(
-  hostlist-compiler 
-  --config=config.json
-  --output=out/blocklist.txt
-)
-
-if [[ -v DEBUG ]]; then
-  cmd+=(--verbose)
-fi
-
+cmd=(hostlist-compiler --config=config.json --output=out/blocklist.txt)
+[[ -v DEBUG ]] && cmd+=(--verbose)
 "${cmd[@]}"
